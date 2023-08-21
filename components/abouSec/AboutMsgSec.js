ebc1dcca5ebb42cc566ab92d0efef7be8a9e994a
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-08-21 14:18:14 +0600

    lasdjflsd

--- components/abouSec/AboutMsgSec.js
+++ components/abouSec/AboutMsgSec.js
@@ -1,40 +1,22 @@
-import Image from "next/image";
-import Container from "../layout/Container";
-import { useForm } from "react-hook-form";
-// import emailjs from "@emailjs/browser";
-import { useRef } from "react";
-import ButtonP from "../layout/ButtonP";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Image from 'next/image';
+import Container from '../layout/Container';
+import ButtonP from '../layout/ButtonP';
 const AboutMsgSec = () => {
-  const { register, handleSubmit, reset } = useForm();
-  const formRef = useRef(null);
- 
-  const onSubmit = () => {
-    // e.preventDefault();
-    // const formData = new FormData();
-    const form = formRef.current;
-    try {
-      emailjs 
-        .sendForm(
-          "service_swmyboi",
-          "template_yxon8gp",
-          form,
-          "ZsPMGCKDpZIhgFLk0"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-            alert("Message sent successfully!");
-            reset();
-          },
-          (error) => {
-            console.log(error.text);
-            alert("An error occurred. Please try again later.");
-          }
-        );
-    } catch (error) {
-      console.log(error);
-      alert("An error occurred. Please try again later.");
-    }
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_swmyboi', 'template_yxon8gp', form.current, 'ZsPMGCKDpZIhgFLk0')
+      .then((result) => {
+          console.log(result.text);
+          alert("Message Sent Successfully")
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
   };
   return (
     <div className="py-8 sm:py-10 md:py-16 lg:py-24">
@@ -50,17 +32,16 @@ const AboutMsgSec = () => {
             </div>
           </div>
           <div className=" w-full sm:w-[45%] xl:w-[50%] mt-12 sm:mt-0">
-          
-             <div className=" leading-[36px]  w-[100%] lg:pt-8 lg:pb-4 sm:leading-[44px] md:leading-[58px]">
+            <div className=" leading-[36px]  w-[100%] lg:pt-8 lg:pb-4 sm:leading-[44px] md:leading-[58px]">
               <h2 className=" text-[28px] sm:text-[36px] md:text-[40px] lg:text-[46px] inline font-pop font-semibold text-primary">
-              LET’S
+                LET’S
               </h2>
               &nbsp;
               <h2 className=" text-[28px] sm:text-[36px] md:text-[40px]  inline lg:text-[46px] font-pop font-semibold yellow-gr2">
-              TALK
+                TALK
               </h2>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} ref={formRef}>
+            <form onSubmit={sendEmail} ref={form}>
               <div className="md:flex gap-x-4 lg:gap-x-8">
                 <div className=" w-full md:w-2/4">
                   <label
@@ -74,7 +55,7 @@ const AboutMsgSec = () => {
                     required
                     className="border text-base md:text-lg border-para w-full p-2 lg:p-4 rounded-lg mt-1 lg:mt-2 mb-4 focus:outline-yellowp"
                     placeholder="Write your name"
-                    {...register("name")}
+                    name="user_name"
                   />
                 </div>
                 <div className="w-full md:w-2/4">
@@ -89,7 +70,7 @@ const AboutMsgSec = () => {
                     required
                     className="border text-base md:text-lg border-para w-full p-2 lg:p-4 rounded-lg mt-1 lg:mt-2 mb-4 focus:outline-yellowp"
                     placeholder="Write your email"
-                    {...register("email")}
+                    name="user_email"
                   />
                 </div>
               </div>
@@ -105,12 +86,10 @@ const AboutMsgSec = () => {
                   required
                   className="border border-para text-base md:text-lg w-full h-[80px] md:h-[120px] lg:h-[200px] px-4 py-2 mt-2 mb-4 rounded-lg focus:outline-yellowp"
                   placeholder="Write us what you want to know"
-                  {...register("message")}
+                  name="message"
                 />
               </div>
-              <ButtonP
-              title="Submit"
-              />
+              <ButtonP title="Submit" />
             </form>
           </div>
         </div>
